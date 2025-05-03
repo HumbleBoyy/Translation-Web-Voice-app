3aeb86435c766e70a7f9343838ae3309eff074b9
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-05-03 14:26:48 +0500

    New COmmit

--- public/app.js
+++ public/app.js
@@ -34,12 +34,72 @@ function loadVoices(){
 //  Trigger loading voices
 speechSynthesis.onvoiceschanged = loadVoices;
 loadVoices();
+
+// Translate Text with serverless function
+async function translateText(text, targetLang) {
+    try{
+        const response = await fetch('/api/translate', {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({
+                text,
+                target: targetLang
+            })
+        })
+
+        if(!response.ok){
+            throw new Error(`Error ${response.status}: ${await response.text()}`)
+        }
+
+        const data = await response.json();
+        return data.data.translations[0].transLatedText;
+    }catch(error){
+       console.error("Translations Error", error)
+    //    alert("Failed to translate text")
+       return text
+    }
+}
+
+function playText(text, voiceIndex){
+    const utterance = new SpeechSynthesisUtterance(text);
+    if(voices[voiceIndex]){
+        utterance.voice = voices[voiceIndex]
+    }
+    speechSynthesis.speak(utterance)
+}
+
 // Play TTS
-playBtn.addEventListener('click', ()=> {
-     const utterance = new SpeechSynthesisUtterance(textInput.value);
-     const selectedVoice = voices[voiceSelect.value];
-     if(selectedVoice) {
-        utterance.voice = selectedVoice
+playBtn.addEventListener('click', async ()=> {
+     const text = textInput.value.trim();
+     const targetLang = languageSelect.value;
+     const selectedVoiceIndex = voiceSelect.value
+
+     if(!text){
+        alert("Please enter some text")
+        return
      }
-     speechSynthesis.speak(utterance)
+
+     try{
+       // Translate Text
+       const translatedText = await translateText(text, targetLang)
+       playText(translatedText, selectedVoiceIndex)
+     
+    }catch(error){
+       console.error("Error during processing", error)
+    //    alert("An Error Occured")
+     }
 })
+
+
+
+// How to read text inside input with google voices
+// playBtn.addEventListener('click',  ()=> {
+//     const utterance = new SpeechSynthesisUtterance(textInput.value);
+//     const selectedVoice = voices[voiceSelect.value];
+//     if(selectedVoice) {
+//        utterance.voice = selectedVoice
+//     }
+//     speechSynthesis.speak(utterance)
+// })
